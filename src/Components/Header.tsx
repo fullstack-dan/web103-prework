c550ba06d2fd9af7b95dd2ec1d3a781031596202
fullstack-dan <aouloudan@icloud.com>
2024-07-14 19:17:18 -0400

    added project files and header

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header>
+            <h1>audio anthology.</h1>
+            <h2>a music enthusiast platform</h2>
+        </header>
+    )
+}
